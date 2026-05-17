ea85bf0a9df60f6000cf566c3450390de4c06010
陈继亮 <chenjl8891@126.com>
2026-05-17 13:35:21 +0800

    fix: wx app id conf of nacos and fix: wx_request_test

--- internal/app/weichat/wx_request_test.go
+++ internal/app/weichat/wx_request_test.go
@@ -1,12 +1,21 @@
 package weichat
 
 import (
+	"op-wx-api/internal/pkg/conf"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // TestGetAccessToken
 func TestGetAccessToken(t *testing.T) {
-	_, err := getAccessToken()
+	var err error
+	err = conf.NacosReadRemoteConfig()
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	_, err = getAccessToken()
 	if err != nil {
 		t.Fatal(err)
 	}
--- internal/pkg/conf/nacos.go
+++ internal/pkg/conf/nacos.go
@@ -2,20 +2,21 @@ package conf
 
 import (
 	"cmp"
+	"os"
+
 	"github.com/chenjl-ops/go-lib/nacos"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
 var NacosConfig *Specification
 
 func NacosReadRemoteConfig() error {
 	nc, err := nacos.NewNacosConfig(
-		nacos.WithUrl(cmp.Or(os.Getenv("RUNTIME_CONFIG_URL"), "http://10.254.253.47")),
+		nacos.WithUrl(cmp.Or(os.Getenv("RUNTIME_CONFIG_URL"), "http://10.1.16.12")),
 		nacos.WithDataId(cmp.Or(os.Getenv("RUNTIME_APP_NAME"), "op-wx-api")),
 		nacos.WithPath("/nacos"),
 		nacos.WithPort(8848),
-		nacos.WithGroup(cmp.Or(os.Getenv("RUNTIME_GROUP"), "test")),
+		nacos.WithGroup(cmp.Or(os.Getenv("RUNTIME_GROUP"), "ops")),
 		nacos.WithTenant(cmp.Or(os.Getenv("RUNTIME_ENV"), "public")),
 	)
 
